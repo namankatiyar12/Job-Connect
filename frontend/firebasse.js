af1afbd0d363e748d59967090aa2bba5e90ea66b
Keshavshhsh <keshavshukla761@gmail.com>
2025-06-27 21:38:10 +0530

    firebase modification

--- frontend/firebasse.js
+++ frontend/firebasse.js
@@ -2,18 +2,16 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB_yFOzw9AJXXmvwkGbHRgPgZpL2YsnhxE",
-  authDomain: "jobconnect-e95b7.firebaseapp.com",
-  projectId: "jobconnect-e95b7",
-  storageBucket: "jobconnect-e95b7.firebasestorage.app",
-  messagingSenderId: "174513197440",
-  appId: "1:174513197440:web:d76be50e728cc68b13b165",
-  measurementId: "G-ZKDJK7RFV6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-const app =
-    initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
 export const googleProvider = new GoogleAuthProvider();
